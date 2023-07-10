73320c5034f4956a9014c6fb9abfc4e1f14ac19e
Carlos <carcamfer@gmail.com>
2023-07-10 17:48:47 -0600

    Final

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-const { hash } = window.location;
+<!DOCTYPE html>
+<html>
+  <head>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+    />
+    <style>
+      .row {
+        margin-top: 25vh;
+      }
 
-console.log(atob(hash.replace('#', '')));
+      h1 {
+        margin-top: 0;
+      }
+    </style>
+  </head>
 
-document.querySelector('form').addEventListener('submit', event => {
-  event.preventDefault();
+  <body>
+    <div class="row">
+      <div class="col s8 offset-s2">
+        <div id="message-show" class="card-panel hide">
+          <h1></h1>
+          <div>
+            <a href="./index.html">Create your own secret message</a>
+          </div>
+        </div>
 
-  document.querySelector('#message-form').classList.add('hide');
-  document.querySelector('#link-form').classList.remove('hide');
+        <div id="message-form" class="card-panel">
+          <form>
+            <label>Enter a Secret Message</label>
+            <input id="message-input" />
+            <button class="btn">Create</button>
+          </form>
+        </div>
 
-  const input = document.querySelector('#message-input');
-  const encrypted = btoa(input.value);
-
-  const linkInput = document.querySelector('#link-input');
-  linkInput.value = `${window.location}#${encrypted}`;
-  linkInput.select();
-});
+        <div id="link-form" class="card-panel hide">
+          <div>
+            Share this link with a friend
+            <input id="link-input" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="index.js"></script>
+  </body>
+</html>
